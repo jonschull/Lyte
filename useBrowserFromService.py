ef3670d84fe4549573241a48b348d3ea05cb532a
jonschull <Jschull@gmail.com>
2018-03-18 10:38:13 -0400

    useBrowserWithService works headless
    converts 
    an unadorned program/glowscript (no first line)  into a  self.contained  webpage
    works headlessly.  runs fast.
    
    needs to be able to deal with adornments
    needs to connect to lyte

--- useBrowserFromService.py
+++ useBrowserFromService.py
@@ -1,12 +1,8 @@
 from chromedriverService import BrowserFromService, Keys
-print('hi')
 from copypaste import write_to_clipboard, read_from_clipboard
 from time import sleep
 
-B = BrowserFromService()
-#from selenium import webdriver
-#B=webdriver.Firefox()
-
+B = BrowserFromService(headless = True)
 
 B.get('http://localhost:8080/_ah/login')
 
@@ -18,44 +14,22 @@ def login():
     input.send_keys('jschull@gmail.com')
     input.send_keys(Keys.TAB*2)
     input.send_keys(Keys.RETURN)
-
 login()
 
 
-
-
-
-def clearOutWorkspace():
-    global textArea
-    B.get('http://localhost:8080/#/user/jschull/folder/Private/program/new/edit')
-    sleep(1)
-    textArea=B.find_element_by_tag_name('textarea')
-    sleep(1)
-    textArea.send_keys(Keys.COMMAND+"a")
-    sleep(1)
-    textArea.send_keys(Keys.BACKSPACE)
-
-#clearOutWorkspace()
-
-trace=True
-
 def srcFromFileName(filename='test.py'):
     """get source, apply transformation"""
-    if trace: print('srcFromFileName',filename)
 
     thesource=open(filename).read()
     
     lines= thesource.split('\n')
     
-##    firstLine = 'GlowScript 2.7 VPython'
-##    if lines[0] != firstLine:
-##        lines.insert(0, firstLine )
-##        
-    lines.insert(1, 'def GlowMe( me=""): pass')
-    lines.insert(2, "get_library('http://localhost:8080/lib/jonlib.js')")
+    lines.insert(1, 'def GlowMe( me=""): pass\n')
+    lines.insert(2, "get_library('http://localhost:8080/lib/jonlib.js')\n")
     fixedLines=[]
+
     for line in lines:
-        
+        #lines beginning with any of these phrases need to commented out 
         toxicStarters = 'GlowMe from import DICT=dict'.split()
         for poison in toxicStarters:
             if line.strip().startswith(poison):
@@ -66,38 +40,62 @@ def srcFromFileName(filename='test.py'):
     return '\n'.join(fixedLines)
 
 def goToWorkspace():
-    global textArea
+    global textarea
     B.get('http://localhost:8080/#/user/jschull/folder/Public/program/workspace/edit')
-    textArea=B.find_element_by_tag_name('textarea')
-
-
-def typeInWorkspace( source = srcFromFileName()):
-    global textArea
-    try:
-        textArea.send_keys('GlowScript 2.7 VPython') #expect trouble
-    except:
-    #fix trouble
-        B.get('http://localhost:8080/#/user/jschull/folder/Public/program/workspace/edit')
-        textArea=B.find_element_by_tag_name('textarea')
-    
-    textArea.send_keys(source)
-    
-    
+    textarea=B.find_element_by_tag_name('textarea')
 
 goToWorkspace()
 #typeToWorkspace()
 from selenium.webdriver import ActionChains
-def pasteToBrowser():
-    actions=ActionChains(B)
-    sleep(1)
-    actions.context_click().send_keys(Keys.ARROW_DOWN).perform() #THIS MAKES PAST WORK.  
-    #actions=ActionChains(B)
-    #actions.send_keys('GlowScript 2.7 VPython').send_keys(Keys.ENTER).perform()
 
-    #THISWORKS
+targetName = 'test.py'
+
+def pasteToBrowser( src ):
+    #select all and delete
+    textarea=B.find_element_by_tag_name('textarea')
+    textarea.send_keys(Keys.COMMAND+"a") #select all
+    textarea.send_keys(Keys.BACKSPACE)
+    
+    #fill clipboad (and right click because it seems to allow things to work (bizarrely)
+    actions=ActionChains(B)
+    actions.context_click().send_keys(Keys.ARROW_DOWN).perform() #THIS MAKES PAST WORK.
+    write_to_clipboard('GlowScript 2.7 VPython\n\n' + src + '\n')
+
+    #paste
     actions=ActionChains(B)
     actions.send_keys(Keys.SHIFT+Keys.INSERT).perform()
+pasteToBrowser( srcFromFileName( targetName ) )
 
-pasteToBrowser()
+def getEmbeddableSrc():
+    B.get('http://localhost:8080/#/user/jschull/folder/Public/program/workspace/share')
+    sleep(1) #allow time for textarea to fill
+    textarea=B.find_element_by_tag_name('textarea')
+    embeddableSrc = B.find_element_by_css_selector('.embedSource').text
+    return embeddableSrc
 
-#>>> input = B.find_element_by_id('prog-new-dialog').find_element_by_tag_name('form').find_element_by_tag_name('input')
+def createHTML( src ): #works but uses glowscript template
+    src = src.split('<![CDATA[//><!--')[1]
+    htmlName = targetName.replace('.py', '.html')
+    f=open( htmlName, 'w' )
+    
+    f.write(f"""
+
+<div id="glowscript" class="glowscript">
+<link type="text/css" href="http://localhost:8080/css/redmond/jquery-ui.custom.css" rel="stylesheet" />
+<link type="text/css" href="http://localhost:8080/css/ide.css" rel="stylesheet" />
+
+<script type="text/javascript" language="javascript" src="http://localhost:8080/lib/jquery/IDE/jquery.min.js"></script>
+<script type="text/javascript" language="javascript" src="http://localhost:8080/lib/jquery/IDE/jquery-ui.custom.min.js"></script>
+<script type="text/javascript"                       src="http://localhost:8080/package/glow.2.7.min.js"></script>
+<script type="text/javascript"                       src="http://localhost:8080/package/RSrun.2.7.min.js"></script>
+
+<script type="text/javascript"><!--//--><![CDATA[//><!--
+
+{src}  """)
+    
+    f.close()
+    
+    print(f'{htmlName} created')
+createHTML( getEmbeddableSrc() )
+
+B.quit()
