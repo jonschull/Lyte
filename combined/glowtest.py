062650098c4c374a138bfe1fe407583e88b7e0c6
jonschull <Jschull@gmail.com>
2018-03-21 11:13:31 -0400

    combined
    vpytohtml now catches and displays glowscript errors
    is styied by lyte.say

---
+++ combined/glowtest.py
@@ -0,0 +1,14 @@
+import lyte
+from vpython import box
+box()
+
+
+lyte.say('this is lyte saying in glowtest.py')
+
+def f():
+    print('this is a function')
+f()
+print("14:51:20 03/20/18 EDT")
+
+D={1:1, 2:2}
+print(f'{D}= {D} {type(D)}' )
---
+++ combined/hi.py
@@ -0,0 +1,8 @@
+import lyte
+
+lyte.blurt('alert for all seasons')
+lyte.say('say what?')
+
+lyte.runRS()
+
+lyte.webMe()
