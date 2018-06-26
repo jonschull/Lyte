0cbbbf79371842db882b6b22128386a0e50d3f66
Jon Schull <jschull@gmail.com>
2018-06-26 17:23:33 -0400

    tweaks

--- notyet.js
+++ notyet.js
@@ -1,20 +1,20 @@
-Promise.all([    pyInstall('utils.py')  ]).then(doMain)
-async function notYet(){
-  source= await getSource('utils.py')
+Promise.all([    pyInstall('utils.py')  ])
+  .then(doMain)
+  .then(notYet)
+
+async function notYet(){ 
+
+    source= await getSource('utils.py')
+    source += await getSource('cone.py')
+    source += await getSource('PPGSOincrementallayout.py')
+    source += await getSource('PPGSOincrementallayout_Main.py')
+
+    await runCode()
+}
 
 
-
-  source += await getSource('cone.py')
-  source += await getSource('PPGSOincrementallayout.py')
-  source += await getSource('PPGSOincrementallayout_Main.py')
-
-
-
-
-  await runCode()
-}notYet()
 function doMain(){window.eval(compiler.compile(`
-
+#put your global imports here
 
 
 
